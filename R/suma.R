63e80fcd66cd22c92c12a78b3a273b90308e7981
reyzaguirre <reyzaguirre@gmail.com>
2016-04-25 08:15:40 -0500

    new function suma

---
+++ R/suma.R
@@ -0,0 +1,22 @@
+#' Compute sum of two traits
+#'
+#' Compute the sum of two traits. Missing values do not propagate.
+#' @param a Name of trait 1 to sum
+#' @param b Name of trait 2 to sum.
+#' @details Missing values do not propagate. If NA is present for both traits then NA
+#' is applied to the sum.
+#' @return It returns the sum of the two traits.
+#' @author Raul Eyzaguirre.
+#' @examples
+#'  # The data
+#'  head(pjpz09)
+#'
+#'  # Compute total biomass as the sum of trw and vw
+#'  suma(pjpz09$trw, pjpz09$vw)
+#' @export
+
+suma <- function(a, b){
+  c <- apply(cbind(a, b), 1, sum, na.rm = T)
+  c[is.na(a) & is.na(b)] <- NA
+  c
+}
