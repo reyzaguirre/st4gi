3345bb62b11b1047ef2e2c49c3a0b6b9c4ea1736
reyzaguirre <reyzaguirre@gmail.com>
2016-04-26 08:46:44 -0500

    new function check.abd

--- R/CheckData.R
+++ R/CheckData.R
@@ -1,3 +1,68 @@
+#' Check data for an ABD
+#'
+#' This function checks the frequencies of genotypes in an ABD.
+#' @param trait The trait to analyze.
+#' @param treat The treatments.
+#' @param rep The replications.
+#' @param data The name of the data frame.
+#' @return A list of treatments \code{newmat}, a list of checks \code{checks},
+#' the number of treatments \code{nt.new}, the number of checks \code{nt.check},
+#' the number of missing values for treatments \code{nmis.new}), the number of missing
+#' values for checks \code{nmis.check}, the number of checks without data \code{nt.check.0},
+#' the number of checks with only one datum \code{nt.check.1}, the number of checks with
+#' at least two data \code{nt.check.2}, and the number of replications \code{nr}.
+#' @author Raul Eyzaguirre.
+#' @details This function checks the frequencies and number of missing values in an ABD.
+#' For an ANOVA in an ABD it is needed at least two checks with at least 2 valid cases each.
+#' @export
+
+check.abd <- function(trait, treat, rep, data) {
+  
+  # Everything as character
+  
+  data[, treat] <- as.character(data[, treat])
+  data[, rep] <- as.character(data[, rep])
+  
+  # Number of replications
+  
+  nr <- nlevels(as.factor(data[, rep]))
+
+  # Identify checks and no checks
+  
+  tfreq <- data.frame(table(data[, treat]))
+  checks <- as.character(subset(tfreq, Freq > 1)[, 1])
+  newmat <- as.character(subset(tfreq, Freq == 1)[, 1])
+  
+  # Number of treatments
+  
+  nt.check <- length(checks)
+  nt.new <- length(newmat)
+  
+  # Number of missing values
+  
+  temp <- subset(data, !(data[, treat] %in% checks))
+  nmis.new <- sum(is.na(temp[, trait]))
+  
+  temp <- subset(data, data[, treat] %in% checks)
+  nmis.check <- sum(is.na(temp[, trait]))
+  
+  # Number of checks without data, and 1 and more data
+
+  temp[, treat] <- as.factor(temp[, treat])
+  temp <- subset(temp, !is.na(temp[, trait]))
+  tfreq <- data.frame(table(temp[, treat]))
+  
+  nt.check.0 <- sum(tfreq$Freq == 0)
+  nt.check.1 <- sum(tfreq$Freq == 1)
+  nt.check.2 <- sum(tfreq$Freq > 1)
+    
+  # Return
+  
+  list(checks = checks, newmat = newmat, nt.check = nt.check, nt.new = nt.new,
+       nmis.check = nmis.check, nmis.new = nmis.new, nr = nr,
+       nt.check.0 = nt.check.0, nt.check.1 = nt.check.1, nt.check.2 = nt.check.2)
+}
+
 #' Check data for a RCBD
 #'
 #' This function checks the frequencies of genotypes in a RCBD.
