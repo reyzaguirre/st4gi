afa1fdf2873e3f975bdab036cc32e7b05a20b4d2
reyzaguirre <reyzaguirre@gmail.com>
2016-05-02 16:05:55 -0500

    solved problem with g+e model

--- R/Elston.R
+++ R/Elston.R
@@ -6,29 +6,33 @@
 #' @param env The environments.
 #' @param rep The replications.
 #' @param data The name of the data frame containing the data.
-#' @param means The genotypic means to compute the index, \code{"single"}
-#' or \code{"fitted"}. The default is \code{"single"}. See details for more information.
-#' @param model Type of model to fit means if \code{means = "fitted"}, \code{"gxe"} for
-#' a model with gxe interaction or \code{"g+e"} for a model without interaction.
+#' @param means The genotypic means to compute the index, \code{"single"} or
+#' \code{"fitted"}. The default is \code{"single"}. See details for more information.
+#' @param model Type of model to fit means if \code{means = "fitted"}: \code{"gxe"}
+#' for a model with gxe interaction or \code{"g+e"} for a model without interaction.
 #' The default is \code{"gxe"}. See details for more information.
 #' @param lb Lower bound. \code{1} for \eqn{k = min(x)} and \code{2} for 
 #' \eqn{k = (n \times min(x) - max(x)) / (n - 1)}
 #' @author Raul Eyzaguirre
 #' @details The Elston index is a weight free index. It is assumed that all the
-#' traits are in the same direction where the highest the value the better the genotype.
-#' To include any trait with an opposite direction it must be transformed by multiplication
-#' by -1 before.
+#' traits are in the same direction where the highest the value the better the
+#' genotype. To include any trait with an opposite direction it must be transformed
+#' by multiplication by \code{-1} before.
 #' 
-#' If \code{means = "fitted"} and \code{model = "gxe"} then the arguments \code{env} and
-#' \code{rep} must be specified.
-#' If \code{means = "fitted"} and \code{model = "g+e"} then only the argument \code{env}
+#' If \code{means = "fitted"} then a linear model is fitted and used to estimate
+#' the means for each genotype. In this case you can choose between a model with
+#' interaction (\code{model = "gxe"}) and without interaction (\code{model = "g+e"}). 
+#' If \code{means = "fitted"} and \code{model = "gxe"} then the arguments \code{env}
+#' and \code{rep} must be specified.
+#' If \code{means = "fitted"} and \code{model = "g+e"} then the argument \code{env}
 #' must be specified.
-#' If \code{means = "single"} and \code{env} and \code{rep} are specified, then
-#' single arithmetic means are computed over the replications for each genotype
-#' at each environment and then for each genotype over environments. In any other case
-#' single arithmetic means are computed over all the observations for each genotype.
-#' @return It returns a data frame with the genotypic means for each trait, the Elston index,
-#' and the rank for each genotype according to the index.
+#' If \code{means = "single"} and \code{env} is specified, then single arithmetic
+#' means are computed over the replications for each genotype at each environment
+#' and then for each genotype over environments.
+#' If \code{means = "single"} and \code{env} is not specified, then single arithmetic
+#' means are computed over all the observations for each genotype.
+#' @return It returns a data frame with the genotypic means for each trait,
+#' the Elston index, and the rank for each genotype according to the index.
 #' @references
 #' Elston, R. C. (1963). A weight-free index for the purpose of ranking or selection
 #' with respect to several traits at a time. Biometrics. 19(1): 85-97.
@@ -46,8 +50,11 @@ elston <- function(traits, geno, env = NULL, rep = NULL, data,
 
   # Error messages
   
-  if (means == "fitted" & (is.null(env) | is.null(rep)))
+  if (means == "fitted" & model == "gxe" & (is.null(env) | is.null(rep)))
     stop("For 'fitted' means with the 'gxe' model you must specify the arguments 'env' and 'rep'.")
+  
+  if (means == "fitted" & model == "g+e" & is.null(env))
+    stop("For 'fitted' means with the 'g+e' model you must specify the argument 'env'.")
   
   # inits
 
@@ -60,18 +67,14 @@ elston <- function(traits, geno, env = NULL, rep = NULL, data,
   outind <- data.frame(geno = levels(factor(data[, geno])))
   colnames(outind) <- geno
   
-  if (means == "single" & (is.null(env) | is.null(rep))) {
-    m <- matrix(NA, ng, nt)
-    for (i in 1:nt)
-      m[, i] <- tapply(data[, traits[i]], data[, geno], mean, na.rm = TRUE)
-    outind <- cbind(outind, m)
+  if (means == "single" & is.null(env)) {
+    outind <- docomp("mean", traits, geno, data = data)
     colnames(outind) <- c("geno", paste("m", traits, sep = "."))
   }
   
-  if (means == "single" & !is.null(env) & !is.null(rep)) {
-    temp <- docomp("mean", traits, c(geno, env), data = data)
-    temp <- docomp("mean", traits, geno, data = temp)
-    outind <- merge(outind, temp, all = TRUE)
+  if (means == "single" & !is.null(env)) {
+    outind <- docomp("mean", traits, c(geno, env), data = data)
+    outind <- docomp("mean", traits, geno, data = outind)
     colnames(outind) <- c("geno", paste("m", traits, sep = "."))
   }
   
