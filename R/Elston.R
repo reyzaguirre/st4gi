89002eb40f8e9c5dbbe7de0585093a49420e6f3a
reyzaguirre <reyzaguirre@gmail.com>
2016-05-06 13:45:58 -0500

    detalle con el nombre de los genotipos

--- R/Elston.R
+++ R/Elston.R
@@ -11,17 +11,17 @@
 #' @param model Type of model to fit means, \code{"gxe"} for a model with gxe interaction
 #' or \code{"g+e"} for a model without interaction. The default is \code{"gxe"}.
 #' See details for more information.
-#' @param lb Lower bound. \code{1} for \eqn{k = min(x)} and \code{2} for 
+#' @param lb Lower bound. \code{1} for \eqn{k = min(x)} and \code{2} for
 #' \eqn{k = (n \times min(x) - max(x)) / (n - 1)}
 #' @author Raul Eyzaguirre
 #' @details The Elston index is a weight free index. It is assumed that all the
 #' traits are in the same direction where the highest the value the better the
 #' genotype. To include any trait with an opposite direction it must be transformed
 #' by multiplication by \code{-1} before.
-#' 
+#'
 #' If \code{means = "fitted"} then a linear model is fitted and used to estimate
 #' the means for each genotype. In this case you can choose between a model with
-#' interaction (\code{model = "gxe"}) and without interaction (\code{model = "g+e"}). 
+#' interaction (\code{model = "gxe"}) and without interaction (\code{model = "g+e"}).
 #' If \code{means = "fitted"} and \code{model = "gxe"} then the arguments \code{env}
 #' and \code{rep} must be specified.
 #' If \code{means = "fitted"} and \code{model = "g+e"} then the argument \code{env}
@@ -48,9 +48,9 @@
 elston <- function(traits, geno, env = NULL, rep = NULL, data,
                    means = c("single", "fitted"), model = c("gxe", "g+e"),
                    lb = 1) {
-  
+
   # match arguments
-  
+
   means <- match.arg(means)
   model <- match.arg(model)
 
@@ -58,10 +58,10 @@ elston <- function(traits, geno, env = NULL, rep = NULL, data,
 
   if (means == "fitted" & model == "gxe" & (is.null(env) | is.null(rep)))
     stop("For 'fitted' means with the 'gxe' model you must specify the arguments 'env' and 'rep'.")
-  
+
   if (means == "fitted" & model == "g+e" & is.null(env))
     stop("For 'fitted' means with the 'g+e' model you must specify the argument 'env'.")
-  
+
   # inits
 
   nt <- length(traits) # number of traits
@@ -69,21 +69,21 @@ elston <- function(traits, geno, env = NULL, rep = NULL, data,
   ng <- nlevels(factor(data[, geno])) # number of genotypes
 
   # compute means
-  
+
   outind <- data.frame(geno = levels(factor(data[, geno])))
   colnames(outind) <- geno
-  
+
   if (means == "single" & is.null(env)) {
     outind <- docomp("mean", traits, geno, data = data)
     colnames(outind) <- c("geno", paste("m", traits, sep = "."))
   }
-  
+
   if (means == "single" & !is.null(env)) {
     outind <- docomp("mean", traits, c(geno, env), data = data)
     outind <- docomp("mean", traits, geno, data = outind)
     colnames(outind) <- c("geno", paste("m", traits, sep = "."))
   }
-  
+
   if (means == "fitted") {
     for (i in 1:nt) {
       if (model == "gxe") {
@@ -97,17 +97,17 @@ elston <- function(traits, geno, env = NULL, rep = NULL, data,
       }
       temp <- as.data.frame(lme4::fixef(fm))
       colnames(temp) <- paste("f", traits[i], sep = ".")
-      temp$geno <- substring(rownames(temp), 5)
+      temp[, geno] <- substring(rownames(temp), nchar(geno) + 1)
       outind <- merge(outind, temp, all = TRUE)
     }
   }
-  
+
   # Standardized means
 
   for (i in 2:(1 + nt))
     outind[, i + nt] <- (outind[, i] - mean(outind[, i], na.rm = TRUE)) / sd(outind[, i], na.rm = TRUE)
   colnames(outind)[(2 + nt):(1 + 2 * nt)] <- c(paste("s", traits, sep = "."))
-  
+
   # compute lower bounds
 
   if (lb == 1)
@@ -125,10 +125,10 @@ elston <- function(traits, geno, env = NULL, rep = NULL, data,
   if (nt > 1)
     for (i in 2:nt)
       outind$E.Index <- outind$E.Index * (outind[, 1 + nt + i] - k[i])
-  
+
   outind <- outind[, c(1:(1 + nt), 2 + 2 * nt)]
   outind$E.Rank <- rank(-outind$E.Index, na.last = "keep")
-  
+
   # results
 
   outind
