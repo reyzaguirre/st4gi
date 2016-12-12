45b03d4078435d42e3705edc0be3d4619047b187
reyzaguirre <reyzaguirre@gmail.com>
2016-12-12 14:25:57 -0500

    revision de datos

--- R/data_1env.R
+++ R/data_1env.R
@@ -6,10 +6,10 @@
 #' number of plants harvested (\code{noph}), number of commercial roots (\code{nocr}),
 #' number of non commercial roots (\code{nonc}), commercial root weight (\code{crw}),
 #' non commercial root weght (\code{ncrw}), total root weight (\code{trw}), and vine
-#' weight (\code{vw}) for an experiment with 102 sweetpotato genotypes (\code{geno})
+#' weight (\code{vw}) for an experiment with 85 sweetpotato genotypes (\code{geno})
 #' and 2 replications (\code{rep}) with a RCBD.
 #' @usage pjpz09
-#' @format A data frame with 10 columns and 204 rows.
+#' @format A data frame with 10 columns and 170 rows.
 #' @source International Potato Center, sweetpotato experimental data.
 NULL
 
