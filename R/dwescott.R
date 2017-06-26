030328205265a483cfce1798a7579e94b6187ae9
reyzaguirre <reyzaguirre@gmail.com>
2017-06-26 14:05:46 -0500

    nc for number of columns ncb for number of columns between

--- R/dwescott.R
+++ R/dwescott.R
@@ -7,7 +7,7 @@
 #' @param ch1 Name of check 1.
 #' @param ch2 Name of check 2.
 #' @param nr Number of rows.
-#' @param nc Number of columns between two check columns.
+#' @param ncb Number of columns between two check columns.
 #' @author Raul Eyzaguirre.
 #' @details The genotypes are randomly allocated on a field with equally spaced
 #' columns of two alternating check varieties.
@@ -20,29 +20,29 @@
 #' dw(1:100, "A", "B", 5, 10)
 #' @export
 
-dw <- function(geno, ch1, ch2, nr, nc) {
+dw <- function(geno, ch1, ch2, nr, ncb) {
   
-  # Check nc is even
+  # Check ncb is even
   
-  if (nc %% 2 == 1)
+  if (ncb %% 2 == 1)
     stop("The number of columns must be even.")
   
-  # Dimentions
+  # Dimensions
   
-  ng <- length(geno)          # Number of genotypes
-  nb <- ceiling(ng / nr / nc) # Number of blocks
-  tnc <- (nc + 1) * nb + 1    # Total number of columns
+  ng <- length(geno)           # Number of genotypes
+  nb <- ceiling(ng / nr / ncb) # Number of blocks
+  nc <- (ncb + 1) * nb + 1     # Number of columns
   
-  # fieldplan array
+  # Fieldplan array
   
-  plan <- array(dim = c(nr, tnc))
+  plan <- array(dim = c(nr, nc))
   
   # Include checks, selected columns
   
-  plan[seq(1, nr, 2), seq(1, tnc, 2 + 2 * nc)] <- ch1
-  plan[seq(2, nr, 2), seq(1, tnc, 2 + 2 * nc)] <- ch2
-  plan[seq(1, nr, 2), seq(2 + nc, tnc, 2 + 2 * nc)] <- ch2
-  plan[seq(2, nr, 2), seq(2 + nc, tnc, 2 + 2 * nc)] <- ch1
+  plan[seq(1, nr, 2), seq(1, nc, 2 + 2 * ncb)] <- ch1
+  plan[seq(2, nr, 2), seq(1, nc, 2 + 2 * ncb)] <- ch2
+  plan[seq(1, nr, 2), seq(2 + ncb, nc, 2 + 2 * ncb)] <- ch2
+  plan[seq(2, nr, 2), seq(2 + ncb, nc, 2 + 2 * ncb)] <- ch1
   
   # Include genotypes at random
   
@@ -51,7 +51,7 @@ dw <- function(geno, ch1, ch2, nr, nc) {
   
   for (b in 1:nb)
     for (i in 1:nr)
-      for (j in (b + 1 + (b - 1) * nc):(b * (1 + nc))) {
+      for (j in (b + 1 + (b - 1) * ncb):(b * (1 + ncb))) {
         if (is.na(plan[i, j])) {
           plan[i, j] <- geno[k]
           k <- k + 1
@@ -60,14 +60,14 @@ dw <- function(geno, ch1, ch2, nr, nc) {
   
   # Delete non necessary checks
   
-  nrc <- sum(!is.na(plan[, tnc - 1])) + 1 # number of rows with clones in the last column
+  nrc <- sum(!is.na(plan[, nc - 1])) + 1 # number of rows with clones in the last column
   if (nrc <= nr)
-    plan[nrc:nr, tnc] <- NA
+    plan[nrc:nr, nc] <- NA
   
   # row and column names
   
   rownames(plan) <- paste("row", 1:nr)
-  colnames(plan) <- paste("col", 1:tnc)
+  colnames(plan) <- paste("col", 1:nc)
 
   # Create fielbook
   
