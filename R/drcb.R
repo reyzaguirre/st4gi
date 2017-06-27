d99193c7415b8586cd9cfdeacf559c42f403eef9
reyzaguirre <reyzaguirre@gmail.com>
2017-06-27 08:24:04 -0500

    genotypes and blocks by rows

--- R/drcb.R
+++ R/drcb.R
@@ -3,23 +3,22 @@
 #' This function creates the fieldbook and fieldplan for a RCBD.
 #' @param geno The list of genotypes.
 #' @param nb Number of blocks.
-#' @param nr Number of rows.
-#' @param byrow Logical. If \code{"TRUE"} (the default) the blocks are
-#' allocated alongside the rows.
+#' @param nc Number of columns.
 #' @author Raul Eyzaguirre.
 #' @details The genotypes are randomly allocated on a field following a RCBD.
+#' The blocks are disposed alongside the rows.
 #' @return It returns the fieldbook and fieldplan.
 #' @examples
-#' drcb(1:20, 3, 12, TRUE)
-#' drcb(1:20, 2, 7, FALSE)
+#' drcb(1:20, 3, 12)
+#' drcb(1:20, 2, 7)
 #' @export
 
-drcb <- function(geno, nb, nr, byrow = TRUE) {
+drcb <- function(geno, nb, nr, byrow = FALSE) {
   
   # Dimensions
   
   ng <- length(geno)      # Number of genotypes
-  nc <- ceiling(ng * nb / nr)  # Number of columns
+  nr <- ceiling(ng * nb / nc)  # Number of rows
   
   # Fieldplan array
   
@@ -33,19 +32,11 @@ drcb <- function(geno, nb, nr, byrow = TRUE) {
 
   k <- 1
   
-  if (byrow == TRUE) {
-    for (i in 1:nr)
-      for (j in 1:nc) {
-        plan[i, j] <- sg[k]
-        k <- k + 1
-      }
-  } else {
-    for (i in 1:nc)
-      for (j in 1:nr) {
-        plan[j, i] <- sg[k]
-        k <- k + 1
-      }
-  }
+  for (i in 1:nr)
+    for (j in 1:nc) {
+      plan[i, j] <- sg[k]
+      k <- k + 1
+    }
   
   # Row and column names
   
@@ -53,22 +44,13 @@ drcb <- function(geno, nb, nr, byrow = TRUE) {
   colnames(plan) <- paste("col", 1:nc)
 
   # Create fielbook
-  
-  if (byrow == TRUE) {
-    row <- as.integer(gl(nr, nc))
-    col <- rep(1:nc, nr)
-    genoplan <- t(plan)
-  } else {
-    row <- rep(1:nr, nc)
-    col <- as.integer(gl(nc, nr))
-    genoplan <- plan
-  }
 
+  row <- as.integer(gl(nr, nc))
+  col <- rep(1:nc, nr)
   block <- as.integer(gl(nb, ng))
   length(block) <- nr * nc
-
-  book <- data.frame(plot = 1:(nr * nc),
-                     row, col, block, geno = c(genoplan), stringsAsFactors = F)
+  book <- data.frame(plot = 1:(nr * nc), row, col, block,
+                     geno = c(t(plan)), stringsAsFactors = F)
   book <- book[!is.na(book$geno), ]
 
   # Return
