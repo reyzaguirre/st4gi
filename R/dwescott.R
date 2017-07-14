32ec65f2641fd18b70c97ed02c628e8df6cee94d
reyzaguirre <reyzaguirre@gmail.com>
2017-07-14 15:53:27 -0500

    wescott with nc instead of nr

--- R/dwescott.R
+++ R/dwescott.R
@@ -6,21 +6,23 @@
 #' @param geno The list of genotypes.
 #' @param ch1 Name of check 1.
 #' @param ch2 Name of check 2.
-#' @param nr Number of rows.
+#' @param nc Number of columns.
 #' @param ncb Number of columns between two check columns (default is 10).
 #' @author Raul Eyzaguirre.
-#' @details The genotypes are randomly allocated on a field with equally spaced
-#' columns of two alternating check varieties.
+#' @details The genotypes are randomly allocated on a field between equally spaced
+#' columns of two alternating check varieties. Check columns are planted each
+#' \code{ncb} columns where \code{ncb} must be even. The specified total number of
+#' columns \code{nc} is the maximum available number, the actual number could be less. 
 #' @return It returns the fieldbook and fieldplan.
 #' @references
 #' Westcott, B. (1981). Two methods for early generation yield assessment in winter wheat.
 #' In: Proc. of the 4th meeting of the Biometrics in Plant Breeding Section of Eucarpia.
 #' INRA Poitier, France, pp 91-95.
 #' @examples
-#' dw(1:100, "A", "B", 5)
+#' dw(1:100, "A", "B", 100)
 #' @export
 
-dw <- function(geno, ch1, ch2, nr, ncb = 10) {
+dw <- function(geno, ch1, ch2, nc, ncb = 10) {
   
   # Check ncb is even
   
@@ -29,9 +31,11 @@ dw <- function(geno, ch1, ch2, nr, ncb = 10) {
   
   # Dimensions
   
-  ng <- length(geno)           # Number of genotypes
-  nb <- ceiling(ng / nr / ncb) # Number of blocks
-  nc <- (ncb + 1) * nb + 1     # Number of columns
+  ng <- length(geno)                 # Number of genotypes
+  nb <- ceiling(ng / ncb)            # Number of blocks
+  nbr <- floor((nc - 1) / (ncb + 1)) # Number of blocks per row
+  nc <- nbr * (ncb + 1) + 1          # Actual number of columns
+  nr <- ceiling(ng / nbr / ncb)      # Number of rows
   
   # Fieldplan array
   
@@ -50,21 +54,19 @@ dw <- function(geno, ch1, ch2, nr, ncb = 10) {
   
   k <- 1
   
-  for (b in 1:nb)
-    for (i in 1:nr)
+  for (i in 1:nr)
+    for (b in 1:nbr)
       for (j in (b + 1 + (b - 1) * ncb):(b * (1 + ncb))) {
-        if (is.na(plan[i, j])) {
-          plan[i, j] <- geno[k]
-          k <- k + 1
-        }
+        plan[i, j] <- geno[k]
+        k <- k + 1
       }
-  
+
   # Delete non necessary checks
   
-  nrc <- sum(!is.na(plan[, nc - 1])) + 1 # number of rows with clones in the last column
-  if (nrc < nr)
-    plan[(nrc + 1):nr, nc] <- NA
-  
+  neb <- nbr * nr - nb  # Number of empty blocks
+  if (neb > 0)
+    plan[nr, (nc - (ncb + 1) * neb + 1):nc] <- NA
+
   # Row and column names
   
   rownames(plan) <- paste("row", 1:nr)
