903d53f1e29ed7bd2d9a855399c2f526c2aa5d60
reyzaguirre <reyzaguirre@gmail.com>
2018-08-01 11:13:59 -0500

    for more than one factor

--- R/ck_fs.R
+++ R/ck_fs.R
@@ -1,23 +1,35 @@
-#' Check number of genotypes and replications
+#' Check factors structure
 #' 
-#' This function cheks the number of genotypes and replications for different designs.
+#' This function cheks the number of treatments and replications for different designs.
 #' 
-#' @param geno The genotypes
+#' @param factors The factors
+#' @param rep The replications
 #' @param design The statistical design
 #' @param dfr The name of the data frame
-#' @return The number of genotypes (\code{ng}), the number of checks (\code{ng.check}),
-#' the list of genotypes (\code{lg}), the list of checks (\code{lg.check}),
-#' and the number of replications (\code{nr}).
+#' @return The number of factors (\code{nf}), the number of levels of the factors
+#' (\code{nl}), the number of treatments (\code{nt}), the number of checks
+#' (\code{nt.check}), the list of treatments (\code{lt}), the list of checks
+#' (\code{lt.check}), and the number of replications (\code{nr}).
 #' @author Raul Eyzaguirre.
 #' @examples 
+#' ## Example 1
 #' # Create a design
 #' dfr <- cr.rcbd(1:20, 3, 10)
 #' dfr <- dfr$book
 #' # Check the design
-#' ck.fs('geno', 'block', 'rcbd', dfr = dfr)
+#' ck.fs("geno", "block", "rcbd", dfr = dfr)
+#' 
+#' ## Example 2
+#' # Create a design
+#' A <- paste0("a", 1:5)
+#' B <- paste0("b", 1:3)
+#' dfr <- cr.f(c("A", "B"), list(A, B), "rcbd", 3, 10)
+#' dfr <- dfr$book
+#' # Check the design
+#' ck.fs(c("A", "B"), "block", "rcbd", dfr = dfr)
 #' @export
 
-ck.fs <- function(geno, rep = NULL, design = c('crd', 'rcbd', 'abd'), dfr) {
+ck.fs <- function(factors, rep = NULL, design = c('crd', 'rcbd', 'abd'), dfr) {
 
   # match arguments
   
@@ -25,21 +37,43 @@ ck.fs <- function(geno, rep = NULL, design = c('crd', 'rcbd', 'abd'), dfr) {
   
   # Check and remove rows with missing values for factors
   
-  dfr <- rm.fna(c(geno, rep), dfr)$dfr
+  dfr <- rm.fna(c(factors, rep), dfr)$dfr
 
-  # Number of genotypes and checks
+  # Number of factors
+  
+  nf <- length(factors)
+  
+  # Number of levels for factors
+  
+  nl <- apply(data.frame(dfr[, factors]), 2, function(x) length(unique(x)))
+  names(nl) <- factors
+  
+  # Define treatments
+  
+  treat <- factors[1]
+  if (nf > 1)
+    for (i in 2:nf)
+      treat <- paste0(treat, factors[i])
+
+  dfr[, treat] <- dfr[, factors[1]]
+  
+  if (nf > 1)
+    for (i in 2:nf)
+      dfr[, treat] <- paste(dfr[, treat], dfr[, factors[i]], sep = "_")
+
+  # Number of treatments and checks
   
   if (design == 'abd') {
-    tfreq <- data.frame(table(dfr[, geno]))
-    lg.check <- as.character(tfreq[tfreq$Freq > 1, 1])
-    lg <- as.character(tfreq[tfreq$Freq == 1, 1])
-    ng.check <- length(lg.check)
-    ng <- length(lg)
+    tfreq <- data.frame(table(dfr[, treat]))
+    lt.check <- as.character(tfreq[tfreq$Freq > 1, 1])
+    lt <- as.character(tfreq[tfreq$Freq == 1, 1])
+    nt.check <- length(lt.check)
+    nt <- length(lt)
   } else {
-    lg <- as.character(unique(dfr[, geno]))
-    ng <- length(lg)
-    lg.check <- NULL
-    ng.check <- NULL
+    lt <- as.character(unique(dfr[, treat]))
+    nt <- length(lt)
+    lt.check <- NULL
+    nt.check <- NULL
   }
   
   # Number of replications
@@ -47,12 +81,13 @@ ck.fs <- function(geno, rep = NULL, design = c('crd', 'rcbd', 'abd'), dfr) {
   if (design %in% c('abd', 'rcbd')) {
     nr <- length(unique(dfr[, rep]))
   } else {
-    tfreq <- table(dfr[, geno])
+    tfreq <- table(dfr[, treat])
     nr <- max(tfreq)
   }
   
   # Return
   
-  list(ng = ng, ng.check = ng.check, lg = lg, lg.check = lg.check, nr = nr)
+  list(nf = nf, nl = nl, nt = nt, nt.check = nt.check,
+       lt = lt, lt.check = lt.check, nr = nr)
   
 }
