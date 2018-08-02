e287f6a1a577ac4dbe69560a284f18f5dc245887
reyzaguirre <reyzaguirre@gmail.com>
2018-08-02 09:15:05 -0500

    using ck.fs

--- R/ck_lxt.R
+++ R/ck_lxt.R
@@ -3,53 +3,48 @@
 #' This function checks the data structure for a line by tester genetic design.
 #' @param line The lines.
 #' @param tester The testers.
-#' @param data The name of the data frame.
-#' @return Three control values (\code{c1}, \code{c2}, and \code{c3},
-#' the number of lines (\code{nl}), and the number of testers (\code{nt}).
-#' @author Raul Eyzaguirre.
-#' @details The control values are:
+#' @param dfr The name of the data frame.
+#' @return Three control values, the number of lines (\code{nlin}),
+#' and the number of testers (\code{ntes}). The control values are:
 #' \itemize{
 #'  \item \code{c1}: TRUE if all lines appear as parents.
 #'  \item \code{c2}: TRUE if all testers appear as parents.
 #'  \item \code{c3}: TRUE if all lines x testers are in the crosses.
 #' }
+#' @author Raul Eyzaguirre.
+#' @examples
+#' library(pepa)
+#' ck.lxt("line", "tester", lxt)
 #' @export
 
-ck.lxt <- function(line, tester, data) {
+ck.lxt <- function(line, tester, dfr) {
   
-  # Get a list of lines and testers
+  # Get names and number of lines, testers, and crosses
   
-  ll <- unique(data[!is.na(data[, line]), line])
-  lt <- unique(data[!is.na(data[, tester]), tester])
+  out <- ck.fs(c("line", "tester"), NULL, dfr)
   
-  # Get number of lines and testers
-  
-  nl <- length(ll)
-  nt <- length(lt)
-  
-  # Get number of crosses
-  
-  temp <- data[!is.na(data[, line]) & !is.na(data[, tester]), ]
-  temp$lxt <- paste(temp[, line], temp[, tester])
-  
-  nlxt <- length(unique(temp$lxt))
-  
+  nlin <- out$nl[1]
+  ntes <- out$nl[2]
+  nlxt <- out$nt
+  llin <- out$lf[[1]]
+  ltes <- out$lf[[2]]
+
   # Check that all lines appear as parents
   
-  temp <- data[is.na(data[, tester]), ]
-  c1 <- identical(ll, unique(temp[!is.na(temp[, line]), line]))
+  temp <- dfr[is.na(dfr[, tester]), ]
+  c1 <- identical(llin, unique(temp[!is.na(temp[, line]), line]))
   
   # Check that all testers appear as parents
   
-  temp <- data[is.na(data[, line]), ]
-  c2 <- identical(lt, unique(temp[!is.na(temp[, tester]), tester]))
+  temp <- dfr[is.na(dfr[, line]), ]
+  c2 <- identical(ltes, unique(temp[!is.na(temp[, tester]), tester]))
   
   # Check that there are nl x nt crosses
   
-  c3 <- nl * nt == nlxt
+  c3 <- as.numeric(nlin * ntes) == nlxt
   
   # Return
   
-  list(c1 = c1, c2 = c2, c3 = c3, nl = nl, nt = nt)
+  list(c1 = c1, c2 = c2, c3 = c3, nlin = nlin, ntes = ntes)
   
 }
