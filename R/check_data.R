27491aa1eb3a80c4905d082cd68f16707c818df1
reyzaguirre <reyzaguirre@gmail.com>
2019-08-27 15:36:35 -0500

    check.data with no crop default

--- R/check_data.R
+++ R/check_data.R
@@ -31,13 +31,11 @@
 #' @importFrom stats IQR quantile rstandard
 #' @export
 
-check.data <- function(dfr, crop = c("pt", "sp"), f = 3,
-                       out.mod = c("none", "rcbd", "met"),
+check.data <- function(dfr, crop, f = 3, out.mod = c("none", "rcbd", "met"),
                        out.max = 4, add = NULL) {
   
   # Match arguments
 
-  crop = match.arg(crop) 
   out.mod = match.arg(out.mod)
   
   # Run check
