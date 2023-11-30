a0a9b9ad78f2f74396c84abee00cc7bb24e258ca
Raul Eyzaguirre <reyzaguirre@gmail.com>
2023-11-30 16:27:40 -0500

    new function create metadata and design/data for spbase

---
+++ R/create_bb.R
@@ -0,0 +1,171 @@
+#' Create sweetpotatobase design and data files
+#'
+#' Creates sweetpotatobase design and phenotypic data files for a set of fieldbooks.
+#' @param metadata The name of the metadata template file.
+#' @details The metadata template can be created with function \code{createmd.sp}.
+#' The fieldbooks should be in memory or as csv files in the working directory,
+#' with the same names specified in the column \code{trial_name} of the metadata,
+#' with standard short labels (see \code{?check.names.sp} for the details).
+#' @return It returns data.frames with names design.file and data.file ready to
+#' upload into sweetpotatobase. Both data.frames should be saved as xlsx files.
+#' @author Raul Eyzaguirre.
+#' @examples
+#' # Create designs
+#' book1 <- cr.rcbd(1:20, 3, 10)$book
+#' book2 <- cr.rcbd(1:20, 3, 10)$book
+#' # Get fieldbook with minimal set of traits
+#' PEP2023CLM_AT01 <- create.fb.sp(book1)
+#' PEP2023CSR_AT02 <- create.fb.sp(book2)
+#' # Create metadata file
+#' metadata <- create.md.sp(trial_name = c('PEP2023CLM_AT01', 'PEP2023CSR_AT02'),
+#'                          breeding_program = 'Peru-CIP',
+#'                          location = c('CLM', 'CSR'),
+#'                          year = 2023,
+#'                          design_type = 'RCBD',
+#'                          description = '20 genotypes with 3 complete blocks',
+#'                          trial_type = 'Advanced Yield Trial')
+#' # Create design and data files for sweetpotatobase
+#' output <- create.bb.sp(metadata)
+#' @importFrom utils read.csv
+#' @export
+
+create.bb.sp <- function(metadata) {
+  
+  # Create meta data file
+  
+  md <- metadata[, c('trial_name', 'breeding_program', 'location',
+                     'year', 'design_type', 'description', 'trial_type',
+                     'plot_width', 'plot_length', 'field_size',
+                     'planting_date', 'harvest_date')]
+  
+  # File names
+  
+  fn <- metadata$trial_name
+  
+  # Create data and design file
+  
+  for (i in 1:length(fn)) {
+    
+    # Look for the fieldbook in the environment
+    # if doesn't exist, try to read from csv file
+    
+    if (exists(fn[i])) {
+      d <- eval(parse(text = fn[i]))
+    } else {
+      d <- read.csv(paste0(fn[i], '.csv'))
+    }
+
+    # Add trial_name column
+    
+    d$trial_name <- fn[i]
+    
+    # Create plot name
+    
+    names(d)[names(d) == 'plot'] <- 'plot_number'
+    d$plot_name <- paste0(d$trial_name, "-0000", d$plot_number)
+    
+    cond <- nchar(d$plot_name) == 24
+    d$plot_name[cond] <- gsub("-000", "-", d$plot_name[cond])
+    cond <- nchar(d$plot_name) == 23
+    d$plot_name[cond] <- gsub("-00", "-", d$plot_name[cond])
+    cond <- nchar(d$plot_name) == 22
+    d$plot_name[cond] <- gsub("-0", "-", d$plot_name[cond])
+    
+    # Block and rep
+    
+    if (md$design[i] %in% c('RRC', 'RCBD')) {
+      if (exists('rep', d)) {
+        names(d)[names(d) == 'rep'] <- 'rep_number'
+        d$block_number <- d$rep_number
+      }
+      if (exists('block', d)) {
+        names(d)[names(d) == 'block'] <- 'block_number'
+        d$rep_number <- d$block_number
+      }
+    }
+
+    # Rename geno
+    
+    names(d)[names(d) == 'geno'] <- 'accession_name'
+    
+    # Row and column
+    
+    if (exists('row', d)) {
+      names(d)[names(d) == 'row'] <- 'row_number'
+    } else {
+      d$row_number <- NA
+    }
+    
+    if (exists('col', d)) {
+      names(d)[names(d) == 'col'] <- 'col_number'
+    } else {
+      d$col_number <- NA
+    }
+    
+    d$range_number <- NA
+    
+    # Checks
+
+    if (!exists('is_a_control', d)) {
+      d$is_a_control <- NA
+      if (exists('type', d)) {
+        d$is_a_control[d$type == 'check'] <- 1
+      }
+    }
+
+    # Additional fields that must be in the design (without data)
+    
+    d$seedlot_name <- NA
+    d$num_seed_per_plot <- NA
+    d$weight_gram_seed_per_plot <- NA 
+    
+    # Design columns from fieldbook
+    
+    design.columns <- c(
+      # Required field from fieldbook to match with md
+      "trial_name",
+      # Required fields from fieldbook
+      "plot_name",	"accession_name",	"plot_number", "block_number",
+      # Additional fields from fieldbook
+      "is_a_control", "rep_number", "range_number", "row_number", "col_number",
+      # Additional fields must be in the template (without data)
+      "seedlot_name", "num_seed_per_plot", "weight_gram_seed_per_plot")
+    
+    # Create design data.frame from fieldbook
+    
+    tmp.design <- d[, design.columns]
+    
+    # Create data data.frame from fieldbook
+    
+    tmp.data <- d[, c("plot_name", colnames(d)[!colnames(d) %in% design.columns])]
+    
+    # Rename as observationunit_name for data file
+    
+    colnames(tmp.data)[colnames(tmp.data) == "plot_name"] <- "observationunit_name"
+    
+    ## Create outputs
+    
+    if (i == 1) {
+      design.file <- tmp.design
+      data.file <- tmp.data
+    } else {
+      design.file <- plyr::rbind.fill(design.file, tmp.design)
+      data.file <- plyr::rbind.fill(data.file, tmp.data)
+    }
+    
+  }
+  
+  # Final design file
+  
+  design.file <- merge(md, design.file)
+
+  # Edit column names for traits
+  
+  data.file <- convert.co.sp(data.file)
+  
+  # output
+  
+ list(design = design.file, data = data.file)
+
+}
+
---
+++ R/create_md.R
@@ -0,0 +1,131 @@
+#' Create metadata template file
+#'
+#' Creates metadata template file for sweetpotatobase upload.
+#' @param folder_name Name of the folder in sweetpotatobase (optional).
+#' @param trial_name Trial name in sweetpotatobase, following convention.
+#' @param breeding_program Breeding program as defined in sweetpotatobase.
+#' @param location Location as defined in sweetpotatobase.
+#' @param year Year (format YYYY)
+#' @param design_type Short hand design type as in sweetpotatobase. See details.
+#' @param description Description of the trial. 
+#' @param trial_type Trial type from options in sweetpotatobase. See details.
+#' @param plot_width The width of the plot in m (optional).
+#' @param plot_length The length of the plot in m (optional).
+#' @param field_size The size of the field in ha (optional).
+#' @param planting_date Planting date (format YYYY-MM-DD).
+#' @param	harvest_date Harvest date (format YYYY-MM-DD).
+#' @param number_of_plants_per_ridge Required to compute tons/ha.
+#' @param number_of_ridges_per_plot Required to compute tons/ha.
+#' @param space_between_ridges Required to compute tons/ha.
+#' @param space_between_plants_in_ridge Required to compute tons/ha.
+#' @param number_plants_per_plot Required to compute tons/ha.
+#' @details Available designs are:
+#' \itemize{
+#'  \item CRD: for Completely Randomized design
+#'  \item RCBD: for Randomized Complete Block design
+#'  \item Alpha: for Alpha Lattice design
+#'  \item Augmented: for Augmented design
+#'  \item MAD: for Modified Augmented design
+#'  \item Westcott: for Westcott design
+#'  \item Lattice: for Lattice design
+#'  \item RRC: for resolvable row-column design
+#'  \item p-rep: for partially replicated design.
+#' }
+#' Available trial types are:
+#' \itemize{
+#'  \item Seedling Nursery
+#'  \item phenotyping_trial
+#'  \item Advanced Yield Trial
+#'  \item Preliminary Yield Trial
+#'  \item Uniform Yield Trial
+#'  \item Variety Release Trial
+#'  \item Lattice: for Lattice design
+#'  \item Clonal Evaluation
+#'  \item genetic_gain_trial
+#'  \item storage_trial 
+#'  \item heterosis_trial
+#'  \item health_status_trial
+#'  \item grafting_trial
+#'  \item crossing_block_trial
+#'  \item Specialty Trial
+#'  \item Seed Multiplication
+#'  \item Screen House
+#'  \item Fodder trial
+#'  \item Sensory Trial
+#'  \item genotyping_project
+#' }
+#' @return It returns a data.frames with metadata to be used with function
+#' \code{create.bb.sp}.
+#' @author Raul Eyzaguirre.
+#' @examples
+#' # Empty metadata file
+#' metadata <- create.md.sp()
+#' # Not empty metadata file
+#' trials <- c('PEP2021BAR-AT03', 'PEP2021CAN-AT03', 'PEP2021PIU-AT03',
+#'             'PEP2021SAT-AT03', 'PEP2021PUC-AT03', 'PEP2021CSR-AT03')
+#' locations <- c('Barranca', 'Cañete', 'Piura', 'Satipo', 'Pucallpa',
+#'                'CIP San Ramon Experimental Station')
+#' description <- 'Number of clones: 35 + 5 checks. Number of reps: 2'
+#' pd <- c('2021-11-13', '2021-08-14', '2021-11-16',
+#'         '2021-12-21', '2021-12-21', '2021-12-21')
+#' hd <- c('2022-03-12', '2021-12-14', '2022-03-18',
+#'         '2022-04-22', '2022-04-19', '2022-04-20')                          
+#' metadata <- create.md.sp(trial_name = trials,
+#'                          breeding_program = 'Peru-CIP',
+#'                          location = locations,
+#'                          year = 2021,
+#'                          design_type = 'RRC',
+#'                          description = description,
+#'                          trial_type = 'Advanced Yield Trial',
+#'                          plot_width = 3.6,
+#'                          plot_length = 5.25,
+#'                          planting_date = pd,
+#'                          harvest_date = hd,
+#'                          number_of_plants_per_ridge = 20,
+#'                          number_of_ridges_per_plot = 4,
+#'                          space_between_ridges = 0.9,
+#'                          space_between_plants_in_ridge = 0.25,
+#'                          number_plants_per_plot = 80)
+#' @export
+
+create.md.sp <- function(folder_name = 'folder_name',
+                        trial_name = NA,
+                        breeding_program = NA,
+                        location = NA,
+                        year = NA,
+                        design_type = NA,
+                        description = NA,
+                        trial_type = NA,
+                        plot_width = NA,
+                        plot_length = NA,
+                        field_size = NA,
+                        planting_date = NA,
+                        harvest_date = NA,
+                        number_of_plants_per_ridge = NA,
+                        number_of_ridges_per_plot = NA,
+                        space_between_ridges = NA,
+                        space_between_plants_in_ridge = NA,
+                        number_plants_per_plot = NA) {
+  
+  metadata <- data.frame(folder_name = folder_name,
+                         trial_name = trial_name,
+                         breeding_program = breeding_program,
+                         location = location,
+                         year = year,
+                         design_type = design_type,
+                         description = description,
+                         trial_type = trial_type,
+                         plot_width = plot_width,
+                         plot_length = plot_length,
+                         field_size = field_size,
+                         planting_date = planting_date,
+                         harvest_date = harvest_date,
+                         number_of_plants_per_ridge = number_of_plants_per_ridge,
+                         number_of_ridges_per_plot = number_of_ridges_per_plot,
+                         space_between_ridges = space_between_ridges,
+                         space_between_plants_in_ridge = space_between_plants_in_ridge,
+                         number_plants_per_plot = number_plants_per_plot)
+  
+  metadata
+  
+}
