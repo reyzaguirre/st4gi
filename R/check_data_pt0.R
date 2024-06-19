db699db7123d6550a11d9c1512b88f4d99d3f343
Raul Eyzaguirre <reyzaguirre@gmail.com>
2024-06-19 13:54:00 -0500

    new function for check data: - incorporates inconsistency matrix in output - items to check go to sysdata Old functionality keeped in fun0 functions

--- R/check_data_pt0.R
+++ R/check_data_pt0.R
@@ -8,62 +8,42 @@
 #' @param out.mod Statistical model for outliers' detection. See details.
 #' @param out.max Threshold for outliers' detection.
 #' @param add Additional quantitative traits.
-#' @param print.text Logical, if \code{TRUE} the output is printed on screen.
+#' @param format Output format as \code{"plain.text"} or \code{"data.frame"}.
 #' @details The data frame must use the labels (lower or upper case) listed in
 #' function \code{check.names.pt}.
 #' 
 #' Extreme low and high values are detected using the interquartile range.
 #' The rule is to detect any value out of the interval 
 #' \eqn{[Q_1 - f \times IQR; Q_3 + f \times IQR]}. By default \code{f = 5}.
-#' If \code{f = 0}, the detection of extreme values is not executed.
 #' 
 #' Outliers are detected based on standardized residuals for some statistical
 #' models. Options are \code{"rcbd"} and \code{"met"} for a randomized complete
 #' block design and a multi environment trial with RCBD in each environment.
 #' By default the threshold value is \code{out.max = 4}.
 #' 
-#' @return It returns:
-#' \itemize{
-#' \item \code{$Inconsist.List}, a \code{data.frame} with a list of all the
-#' rows with some kind of inconsistency.
-#' \item \code{$Inconsist.Matrix}, a \code{data.frame} with the positions
-#' in the fieldbook data frame where inconsistencies occur. These are coded
-#' with: (1) for inconsistencies among traits, (2) for out of range values,
-#' (3) for extreme values or outliers.
-#' }
-#' @author Raul Eyzaguirre.
+#' @return It returns all rows with some kind of inconsistency or outliers.
+#' @author Johan Ninanya, Raul Eyzaguirre.
 #' @examples
-#' check.data.pt(potatoyield)
+#' check.data.pt0(potatoyield)
 #' @importFrom stats IQR quantile rstandard
 #' @export
 
-check.data.pt <- function(dfr, f = 5, out.mod = c("none", "rcbd", "met"),
-                          out.max = 4, add = NULL, print.text = TRUE) {
+check.data.pt0 <- function(dfr, f = 5, out.mod = c("none", "rcbd", "met"),
+                          out.max = 4, add = NULL,
+                          format = c("plain.text", "data.frame")) {
   
   # Match arguments
   
   out.mod = match.arg(out.mod)
-
-  # Check names
-  
-  dfr <- check.names.pt(dfr, add)
-  if (!is.null(add))
-    add <- tolower(add)
-  
-  # Create inconsistencies matrix
-  
-  im <- structure(rep(0, dim(dfr)[1] * dim(dfr)[2]),
-                  .Dim = c(dim(dfr)[1], dim(dfr)[2]),
-                  .Dimnames = list(NULL, names(dfr)))
+  format = match.arg(format)
   
   # Run check
   
-  output <- rules.pt(dfr, im, f, out.mod, out.max, add, print.text)
+  dfr.out <- rules.pt0(dfr, f, out.mod, out.max, add, format)
   
-  if (dim(output$Inconsist.List)[1] > 0) {
-    rownames(output$Inconsist.List) <- 1:dim(output$Inconsist.List)[1]
+  if (format == 'data.frame' & dim(dfr.out)[1] > 0) {
+    rownames(dfr.out) <- 1:dim(dfr.out)[1]
+    dfr.out
   }
-
-  invisible(output)
   
 }
--- R/check_data_sp.R
+++ R/check_data_sp.R
@@ -8,20 +8,29 @@
 #' @param out.mod Statistical model for outliers' detection. See details.
 #' @param out.max Threshold for outliers' detection.
 #' @param add Additional quantitative traits.
-#' @param format Output format as \code{"plain.text"} or \code{"data.frame"}.
+#' @param print.text Logical, if \code{TRUE} the output is printed on screen.
 #' @details The data frame must use the labels (lower or upper case) listed in
 #' function \code{check.names.sp}.
 #' 
 #' Extreme low and high values are detected using the interquartile range.
 #' The rule is to detect any value out of the interval 
 #' \eqn{[Q_1 - f \times IQR; Q_3 + f \times IQR]}. By default \code{f = 5}.
+#' If \code{f = 0}, the detection of extreme values is not executed.
 #' 
 #' Outliers are detected based on standardized residuals for some statistical
 #' models. Options are \code{"rcbd"} and \code{"met"} for a randomized complete
-#' block design and a multi environment trial with RCBD in each environment.
+#' block design and a multienvironment trial with RCBD in each environment.
 #' By default the threshold value is \code{out.max = 4}.
 #' 
-#' @return It returns all rows with some kind of inconsistency or outliers.
+#' @return It returns:
+#' \itemize{
+#' \item \code{$Inconsist.List}, a \code{data.frame} with a list of all the
+#' rows with some kind of inconsistency.
+#' \item \code{$Inconsist.Matrix}, a \code{data.frame} with the positions
+#' in the fieldbook data frame where inconsistencies occur. These are coded
+#' with: (1) for inconsistencies among traits, (2) for out of range values,
+#' (3) for extreme values or outliers.
+#' }
 #' @author Raul Eyzaguirre.
 #' @examples
 #' check.data.sp(pjpz09)
@@ -29,21 +38,69 @@
 #' @export
 
 check.data.sp <- function(dfr, f = 5, out.mod = c("none", "rcbd", "met"),
-                          out.max = 4, add = NULL,
-                          format = c("plain.text", "data.frame")) {
+                          out.max = 4, add = NULL, print.text = TRUE) {
   
   # Match arguments
   
   out.mod = match.arg(out.mod)
-  format = match.arg(format)
+
+  # Check names
+  
+  dfr <- check.names.sp(dfr, add)
+  if (!is.null(add))
+    add <- tolower(add)
+  
+  # Compute trw with NA omited
+  
+  if (exists("crw", dfr) & !exists("ncrw", dfr))
+    dfr$trw <- dfr$crw
+  if (!exists("crw", dfr) & exists("ncrw", dfr))
+    dfr$trw <- dfr$ncrw
+  if (exists("crw", dfr) & exists("ncrw", dfr)) {
+    dfr$trw <- apply(cbind(dfr$crw, dfr$ncrw), 1, sum, na.rm = TRUE)
+    dfr[is.na(dfr$crw) & is.na(dfr$ncrw), "trw"] <- NA
+    dfr[is.na(dfr$crw) & !is.na(dfr$ncrw) & dfr$ncrw == 0, "trw"] <- NA
+    dfr[!is.na(dfr$crw) & dfr$crw == 0 & is.na(dfr$ncrw), "trw"] <- NA
+  }
+  
+  # Compute tnr with NA omited
+  
+  if (exists("nocr", dfr) & !exists("nonc", dfr))
+    dfr$tnr <- dfr$nocr
+  if (!exists("nocr", dfr) & exists("nonc", dfr))
+    dfr$tnr <- dfr$nonc
+  if (exists("nocr", dfr) & exists("nonc", dfr)) {
+    dfr$tnr <- apply(cbind(dfr$nocr, dfr$nonc), 1, sum, na.rm = TRUE)
+    dfr[is.na(dfr$nocr) & is.na(dfr$nonc), "tnr"] <- NA
+    dfr[is.na(dfr$nocr) & !is.na(dfr$nonc) & dfr$nonc == 0, "tnr"] <- NA
+    dfr[!is.na(dfr$nocr) & dfr$nocr == 0 & is.na(dfr$nonc), "tnr"] <- NA
+  }
+
+  # Transform dmvf, dmvd, dmf, and dmd to kilograms
+  
+  if (exists("dmvf", dfr))
+    dfr$dmvf <- dfr$dmvf / 1000
+  if (exists("dmvd", dfr))
+    dfr$dmvd <- dfr$dmvd / 1000
+  if (exists("dmf", dfr))
+    dfr$dmf <- dfr$dmf / 1000
+  if (exists("dmd", dfr))
+    dfr$dmd <- dfr$dmd / 1000
+  
+  # Create inconsistencies matrix
+  
+  im <- structure(rep(0, dim(dfr)[1] * dim(dfr)[2]),
+                  .Dim = c(dim(dfr)[1], dim(dfr)[2]),
+                  .Dimnames = list(NULL, names(dfr)))
   
   # Run check
   
-  dfr.out <- rules.sp(dfr, f, out.mod, out.max, add, format)
+  output <- rules.sp(dfr, im, f, out.mod, out.max, add, print.text)
   
-  if (format == 'data.frame' & dim(dfr.out)[1] > 0) {
-    rownames(dfr.out) <- 1:dim(dfr.out)[1]
-    dfr.out
+  if (dim(output$Inconsist.List)[1] > 0) {
+    rownames(output$Inconsist.List) <- 1:dim(output$Inconsist.List)[1]
   }
+  
+  invisible(output)
   
 }
